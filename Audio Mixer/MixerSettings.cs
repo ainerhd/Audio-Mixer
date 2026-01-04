ce05848dd9ceda7dfda4dcd853686ed58c16c4a3
ainerhd <amin.bouklata@gmail.com>
2026-01-04 07:36:09 +0100

    Add settings tab for ports colors and channel sizing

--- Audio Mixer/MixerSettings.cs
+++ Audio Mixer/MixerSettings.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace Audio_Mixer
 {
@@ -7,6 +8,15 @@ namespace Audio_Mixer
         public int ChannelCount { get; set; } = 5;
         public int Deadzone { get; set; } = 6;
         public List<ChannelSettings> Channels { get; set; } = new();
+        public bool ManualPortEnabled { get; set; }
+        public string? ManualPortName { get; set; }
+        public int BackgroundColorArgb { get; set; }
+        public int SurfaceColorArgb { get; set; }
+        public int SurfaceAccentColorArgb { get; set; }
+        public int AccentColorArgb { get; set; }
+        public int MutedTextColorArgb { get; set; }
+        public int ChannelLabelWidth { get; set; } = 120;
+        public int ChannelRowHeight { get; set; } = 52;
 
         public static MixerSettings CreateDefault()
         {
@@ -14,6 +24,13 @@ namespace Audio_Mixer
             {
                 ChannelCount = 5,
                 Deadzone = 6,
+                BackgroundColorArgb = Color.FromArgb(24, 24, 28).ToArgb(),
+                SurfaceColorArgb = Color.FromArgb(36, 36, 42).ToArgb(),
+                SurfaceAccentColorArgb = Color.FromArgb(44, 44, 52).ToArgb(),
+                AccentColorArgb = Color.FromArgb(88, 142, 206).ToArgb(),
+                MutedTextColorArgb = Color.FromArgb(180, 182, 190).ToArgb(),
+                ChannelLabelWidth = 120,
+                ChannelRowHeight = 52,
             };
         }
     }
